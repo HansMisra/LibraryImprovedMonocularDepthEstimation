208b8c3726fb718e5b84511eb6186a9b728d1a35
Hans Misra <hmisra@umd.edu>
2026-05-24 10:41:22 -0400

    touch up on disp

--- src_code/create_test_disp.py
+++ src_code/create_test_disp.py
@@ -69,7 +69,5 @@ image_dir1 = os.path.join('kitti_data', 'data_scene_flow', 'testing', 'image_2')
 image_dir2 = os.path.join('kitti_data', 'data_scene_flow', 'testing', 'image_3')
 output_dir = os.path.join('kitti_data', 'data_scene_flow', 'testing', 'test_disp')
 
-save_disparity_maps(image_dir1, image_dir2, output_dir)
-
 if __name__ == "__main__":
     save_disparity_maps(image_dir1, image_dir2, output_dir)
